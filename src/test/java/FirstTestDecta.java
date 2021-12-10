36561ab74ec6e3418dccbb574c2ec7a779264d4b
Vjaceslavs Jermakovs <vjaceslavs.jermakovs@decta.com>
2021-12-10 18:36:15 +0200

    addition loggers to the test

--- src/test/java/FirstTestDecta.java
+++ src/test/java/FirstTestDecta.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FirstTestDecta {
 
-        //  .//*[contains(@name, 'username')]
     private final By LOGIN_FIELD = By.name("username");
     private final By PASSWORD_FIELD = By.name("password");
     private final By LOG_IN = By.xpath(".//div[@class = 's-flex']");
@@ -38,12 +37,9 @@ public class FirstTestDecta {
     private final By SAVE_TEMPLATE_BTN = By.xpath(".//footer/div/nav/a/span[2]");
     private final By INVOICE_TOTAL_PRICE = By.xpath(".//div[@class = 's-flex s-text-size--l']/div[2]");
     private final By ADD_INVOICE_PAGE_INVOICE_NAME = By.xpath(".//div/div/label/input");
-        //private final By ADD_INVOICE_PAGE_INVOICE_SECTION = By.xpath(".//li[@class = 's-padding-right--xs active']/a/span[2]");
-        //private final By INVOICE_POPUP_LEAVE_BTN = By.xpath(".//div[contains(@class, 'ui-dialog')]/footer/a[2]");
 
     private final By CREATED_INVOICE = By.xpath(".//div[@class = 's-wrapper']/div/div/div/div/a[1]");
 
-        //private final By EDIT_INV_INV_NAME = By.xpath(".//div[@class = 'formRow']/label/input[@value = 'Automation testing invoice']");
     private final By DELETE_TEMPLATE = By.xpath(".//footer/div/nav/a[1]/span[2]");
     private final By POPUP_DELETE_TEMPLATE = By.xpath(".//div[contains(@class, 'ui-dialog ui-corner-all')]/footer/a[2]");
     private final By PRODUCTS_SECTION = By.xpath(".//ul/li[8]");
@@ -51,9 +47,12 @@ public class FirstTestDecta {
     private final By DELETE_PRODUCTS = By.xpath(".//footer/div/nav/a/span[2]");
     private final By POPUP_DELETE_PRODUCT = By.xpath(".//div[contains(@class, 'ui-dialog')]/footer/a[2]");
 
+
     private final Logger LOGGER = LogManager.getLogger(FirstTestDecta.class);
 
+
     private WebDriver driver;
+
 
     @Test
     public void firstTest() throws InterruptedException {
@@ -61,17 +60,16 @@ public class FirstTestDecta {
 
         LOGGER.info("Setting driver location");
         System.setProperty("webdriver.chrome.driver", "C://Users/vjermakovs/Desktop/New folder/chromedriver.exe");
-
         LOGGER.info("Opening browser window");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
         LOGGER.info("Opening Home Page");
         driver.get("https://gate.dcdev.dvte.ch/merchants/en/login");
 
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
         JavascriptExecutor scrollDown = (JavascriptExecutor) driver;
+
 
         LOGGER.info("Looking for Login field to click on it on the GATE LOGIN page");
         wait.until(ExpectedConditions.presenceOfElementLocated(LOGIN_FIELD));
@@ -164,25 +162,17 @@ public class FirstTestDecta {
         LOGGER.info("Scrolling down the page and Looking for Invoice Total price then to save it for the Assertions");
         scrollDown.executeScript("window.scrollBy(0, 400)");
         String addInvoicePageTotalPrice = driver.findElement(INVOICE_TOTAL_PRICE).getText();
-
-            //String addInvoicePageTotalPriceString = driver.findElement(ADD_INVOICE_PAGE_TOTAL_PRICE).getText();
-            //int addInvoicePageTotalPrice = Integer.parseInt(addInvoicePageTotalPriceString);
-
         LOGGER.info("Looking for Save Template button to click on it");
         WebElement saveTemplateBtn = driver.findElement(SAVE_TEMPLATE_BTN);
         saveTemplateBtn.click();
         LOGGER.info("Gate need the time to save all changes so i am waiting fo 3 sec");
         Thread.sleep(3000); // gate need time to save all changes
-        LOGGER.info("Looking for Add Invoice Section to click on it");
+        LOGGER.info("Looking for Invoice Section to click on it");
         WebElement addInvoiceInvoiceSection = driver.findElement(INVOICE_SECTION);
         addInvoiceInvoiceSection.click();
 
-            //wait.until(ExpectedConditions.presenceOfElementLocated(INVOICE_POPUP_LEAVE_BTN));
-            //WebElement invoicePageLeaveBtn = driver.findElement(INVOICE_POPUP_LEAVE_BTN);
-            //wait.until(ExpectedConditions.elementToBeClickable(INVOICE_POPUP_LEAVE_BTN));
-            //invoicePageLeaveBtn.click();
 
-        LOGGER.info("Looking for Created invoice to click on it on the Invoice page");
+        LOGGER.info("Looking for Created invoice to click on it on the INVOICE PAGE");
         wait.until(ExpectedConditions.presenceOfElementLocated(CREATED_INVOICE));
         WebElement createdInvoice = driver.findElement(CREATED_INVOICE);
         createdInvoice.click();
@@ -195,33 +185,42 @@ public class FirstTestDecta {
         scrollDown.executeScript("window.scrollBy(0, 500)");
         wait.until(ExpectedConditions.presenceOfElementLocated(INVOICE_TOTAL_PRICE));
         String editInvoicePageTotalPrice = driver.findElement(INVOICE_TOTAL_PRICE).getText();
+        LOGGER.info("Looking for Delete template button to click on it");
         WebElement deleteTemplate = driver.findElement(DELETE_TEMPLATE);
         deleteTemplate.click();
+        LOGGER.info("Looking for Yes button on the POPUP to click on it and confirm that we are sure that we want to delete Template");
         wait.until(ExpectedConditions.presenceOfElementLocated(POPUP_DELETE_TEMPLATE));
         WebElement popupDeleteInvoices = driver.findElement(POPUP_DELETE_TEMPLATE);
         popupDeleteInvoices.click();
+        LOGGER.info("Gate need the time to save all changes so i am waiting fo 3 sec");
         Thread.sleep(3000); // gate need time to save all changes
 
+
+        LOGGER.info("Looking for Product section to click on it on the INVOICE PAGE");
         wait.until(ExpectedConditions.presenceOfElementLocated(PRODUCTS_SECTION));
         WebElement productsSection = driver.findElement(PRODUCTS_SECTION);
         productsSection.click();
+
+
+        LOGGER.info("Looking for Product Edit button to click on it on the PRODUCTS PAGE");
         wait.until(ExpectedConditions.presenceOfElementLocated(PRODUCTS_EDIT));
         WebElement productEdit = driver.findElement(PRODUCTS_EDIT);
         productEdit.click();
 
+
+        LOGGER.info("Looking for Delete product button to click on it on the EDIT PRODUCTS PAGE");
         wait.until(ExpectedConditions.presenceOfElementLocated(DELETE_PRODUCTS));
         WebElement deleteProducts = driver.findElement(DELETE_PRODUCTS);
         deleteProducts.click();
+        LOGGER.info("Looking for Yes button on the POPUP to click on it and confirm that we are sure that we want to delete Product");
         wait.until(ExpectedConditions.elementToBeClickable(POPUP_DELETE_PRODUCT));
         WebElement popDeleteProductBtn = driver.findElement(POPUP_DELETE_PRODUCT);
         popDeleteProductBtn.click();
 
 
-            //wait.until(ExpectedConditions.presenceOfElementLocated(INVOICE_SECTION));
-            //WebElement editInvoicePageInvoiceSection = driver.findElement(INVOICE_SECTION);
-            //editInvoicePageInvoiceSection.click();
-
+        LOGGER.info("Compare Invoice name from the ADD INVOICE PAGE and EDIT INVOICE PAGE");
         Assertions.assertEquals(addInvoicePageInvoiceName, editInvoicePageInvoiceName, "Wrong invoice name!");
+        LOGGER.info("Compare Invoice Total price from the ADD INVOICE PAGE and EDIT INVOICE PAGE");
         Assertions.assertEquals(addInvoicePageTotalPrice, editInvoicePageTotalPrice, "Wrong total price!");
     }
 
@@ -231,3 +230,23 @@ public class FirstTestDecta {
         driver.close();
    }
 }
+
+//  .//*[contains(@name, 'username')]
+//private final By ADD_INVOICE_PAGE_INVOICE_SECTION = By.xpath(".//li[@class = 's-padding-right--xs active']/a/span[2]");
+//private final By INVOICE_POPUP_LEAVE_BTN = By.xpath(".//div[contains(@class, 'ui-dialog')]/footer/a[2]");
+//private final By EDIT_INV_INV_NAME = By.xpath(".//div[@class = 'formRow']/label/input[@value = 'Automation testing invoice']");
+
+//-----Add invoice page--------
+//String addInvoicePageTotalPriceString = driver.findElement(ADD_INVOICE_PAGE_TOTAL_PRICE).getText();
+//int addInvoicePageTotalPrice = Integer.parseInt(addInvoicePageTotalPriceString);
+
+
+//wait.until(ExpectedConditions.presenceOfElementLocated(INVOICE_POPUP_LEAVE_BTN));
+//WebElement invoicePageLeaveBtn = driver.findElement(INVOICE_POPUP_LEAVE_BTN);
+//wait.until(ExpectedConditions.elementToBeClickable(INVOICE_POPUP_LEAVE_BTN));
+//invoicePageLeaveBtn.click();
+
+
+//wait.until(ExpectedConditions.presenceOfElementLocated(INVOICE_SECTION));
+//WebElement editInvoicePageInvoiceSection = driver.findElement(INVOICE_SECTION);
+//editInvoicePageInvoiceSection.click();
